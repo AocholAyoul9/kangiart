f0ac1ee19543fab24d494af7364317b5ce25e3aa
shawil Tech <shawilTech@gmail.com>
2025-07-16 14:45:48 +0200

    working on home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Contact } from './pages/contact/contact';
+import { Gallery } from './pages/gallery/gallery';
+import { About } from './pages/about/about';
+import { Blog } from './pages/blog/blog';
+import { BlogDetail } from './pages/blog-detail/blog-detail';
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+   {
+    path: 'contact',
+    component: Contact,
+  },
+   {
+    path: 'gallery',
+    component: Gallery,
+  },
+   {
+    path: 'about',
+    component: About,
+  },
+   {
+    path: 'blog',
+    component: Blog,
+  },
+   {
+    path: 'blog-detail',
+    component: BlogDetail,
+  },
+];
---
+++ src/app/pages/art-detail/art-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-art-detail',
+  imports: [],
+  templateUrl: './art-detail.html',
+  styleUrl: './art-detail.css'
+})
+export class ArtDetail {
+
+}
