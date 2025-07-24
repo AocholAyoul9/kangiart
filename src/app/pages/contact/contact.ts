0fd8c104e81f0fbbe6155265c9d5644218864657
Ayoul Aochol <ocholayul@gmail.com>
2025-07-24 12:50:40 +0200

    conecting contact form to emailjs

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import emailjs from '@emailjs/browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.css'
+  styleUrl: './contact.css',
 })
 export class Contact {
+  form = {
+    user_name: '',
+    user_email: '',
+    subject: '',
+    message: '',
+  };
 
+  sendEmail() {
+    emailjs
+      .send(
+        'service_hr64wgw',
+        'template_ezrie6k',
+        this.form,
+        'nDlGAVa1AnbUOZHoQ'
+      )
+      .then(() => {
+        alert('Email sent successfully!');
+        this.form = {
+          user_name: '',
+          user_email: '',
+          subject: '',
+          message: '',
+        };
+      })
+      .catch((error) => {
+        console.error('Failed to send email:', error);
+        alert('Failed to send email. Please try again.');
+      });
+  }
 }
